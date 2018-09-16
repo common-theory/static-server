a46acd7a119cb4c24a282416f023161df464604e
Chance Hudson <jchancehud@gmail.com>
2018-09-16 15:59:25 -0500

    Update ipfs response

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 
 ipfs init
 ipfs daemon &
-nginx -g "daemon off;" &
+node /server/build/server.js &
 
 while sleep 20; do
-  ps ax | grep nginx | grep -q -v grep
+  ps ax | grep node | grep -q -v grep
   if [ $? -ne 0 ]; then
-    echo "NGINX process exited"
+    echo "Node process exited"
     exit 1
   fi
   ps ax | grep ipfs | grep -q -v grep
