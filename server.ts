e5ab2330b877c217e54b378d6391490e882b1216
Chance Hudson <jchancehud@gmail.com>
2018-09-17 00:41:42 -0500

    Add comments

--- server.ts
+++ server.ts
@@ -3,9 +3,17 @@ import fs from 'fs';
 import path from 'path';
 
 const MAPPINGS_PATH = path.resolve(__dirname, '../mappings.json');
+
+/**
+ * A key value mapping of domains -> ipfs addresses.
+ **/
 let DNS_MAPPINGS: {
   [key: string]: string
 } = {};
+
+/**
+ * Load the mappings and preload the content into memory.
+ **/
 loadMappings()
   .then(async (mappings: any) => {
     DNS_MAPPINGS = mappings;
@@ -13,10 +21,15 @@ loadMappings()
   })
   .catch(err => console.log('Error in initial load', err));
 
+// Cache some IPFS files in memory
 const cache: {
   [key: string]: string
 } = {};
 
+/**
+ * Watch the mapping json file for changes; when one occurs load the new mapping
+ * into memory and attempt to load the contents into the cache
+ **/
 fs.watch(MAPPINGS_PATH, async () => {
   console.log('Reloading DNS mappings');
   setTimeout(async () => {
@@ -30,6 +43,10 @@ fs.watch(MAPPINGS_PATH, async () => {
   }, 500);
 });
 
+/**
+ * Returns a promise resolving with the JSON data, or an error if either loading
+ * or JSON parsing fails.
+ **/
 function loadMappings(): Promise<{
   [key: string]: string
 }> {
@@ -50,6 +67,9 @@ function loadMappings(): Promise<{
   });
 }
 
+/**
+ * Load all IPFS addresses specified in the currently loaded mapping.
+ **/
 async function preloadAddresses() {
   Object.keys(DNS_MAPPINGS).forEach(async (key) => {
     const address = DNS_MAPPINGS[key];
@@ -62,6 +82,10 @@ async function preloadAddresses() {
   });
 }
 
+/**
+ * The http server that proxies requests to IPFS resources. Mapping are done by
+ * hostname -> ipfs address. These are stored in the mappings.json file.
+ **/
 const server = http.createServer(async (req, res) => {
   const ipfsAddress = DNS_MAPPINGS[req.headers.host];
   if (!ipfsAddress) {
@@ -83,10 +107,16 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
+/**
+ * Begin listening on port 3000.
+ **/
 server.listen(3000, () => {
   console.log('http server listening on port 3000');
 });
 
+/**
+ * Attempts to load a given ipfs address. Results are stored in the cache.
+ **/
 function download(ipfsAddress: string) {
   return new Promise((rs, rj) => {
     if (cache[ipfsAddress]) {
